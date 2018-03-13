14691030bd796e177a9da3ac7b6d41d50330b7db
Yassin Mziya <ymm22@cornell.edu>
2018-03-12 20:22:43 -0400

    iso 3 codes

--- routes/gii.js
+++ routes/gii.js
@@ -3,6 +3,7 @@ var d3 = require('d3');
 class GII {
     constructor(year) {
         this.year = year
+        this.country_codes = d3.csvParse(require(`./common/iso3`))
         this.data = d3.csvParse(require(`./common/${year}`))
     }
 
@@ -12,8 +13,16 @@ class GII {
     }
 
     // given a valid iso3 string, will return JSON data for corresponding country for this year
-    getCountry(iso3) {
+    getCountryData(iso3) {
+        // need to handle errors i.e. incorrect iso3
         var data = this.data.filter(country => country.ISO3 === iso3.toUpperCase())
+        return data[0]
+    }
+
+    // given an iso3 code, returns the corresponding country name
+    getCountry(iso3) {
+        //need to handle errors
+        var data = this.country_codes.filter(country => country.ISO3 === iso3.toUpperCase())
         return data[0]
     }
 }
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,16 @@ router.get('/v1/data/:year', function(req, res) {
 
 router.get('/v1/data/:iso3/:year', function(req, res) {
   var gii = new GII(req.params.year)
-  res.send(gii.getCountry(req.params.iso3))
+  res.send({
+    data:gii.getCountryData(req.params.iso3)
+  })
 });
+
+router.get('/v1/codes/:iso3', function(req, res) {
+  var codes = d3.csvParse(require('./common/iso3'))
+  res.send({
+    iso3 : codes[0][req.params.iso3.toUpperCase()]
+  })
+})
 
 module.exports = router;
